3b37c049232aca02972f149087cafddbc3803fe7
Jim Dai <jim.dai@autoliv.com>
2017-09-20 15:37:38 -0400

    Try to switch between Area and Pane, it failed. The Pane will cover the area which makes not fully visible.

--- Reference/TextAreaPane.java
+++ Reference/TextAreaPane.java
@@ -18,13 +18,13 @@ public class TextAreaPane {
    
    public TextAreaPane() throws BadLocationException {
 		JFrame frame = new JFrame();
-		DefaultStyledDocument document = new DefaultStyledDocument();
-        JTextArea area = new JTextArea();
-		JTextPane pane = new JTextPane(document);
+		JTextArea area = new JTextArea();
+		JTextPane pane = new JTextPane();
         JPanel mainPanel = new JPanel();
 		String buttonDisplay = "Save";
 		JButton button = new JButton(buttonDisplay);
         button.setPreferredSize(new Dimension(100, 40));
+		mainPanel.add(button);
 		
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         area.setPreferredSize(new Dimension(200, 200));		
@@ -32,12 +32,19 @@ public class TextAreaPane {
         paneScrollPane.setVerticalScrollBarPolicy(
                         JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         paneScrollPane.setPreferredSize(new Dimension(250, 155));
-        paneScrollPane.setMinimumSize(new Dimension(10, 10));
-        mainPanel.add(button);
+        paneScrollPane.setMinimumSize(new Dimension(10, 10));        
         frame.getContentPane().add(paneScrollPane, BorderLayout.CENTER);
-        frame.getContentPane().add(mainPanel, BorderLayout.WEST);
+		  frame.getContentPane().add(mainPanel, BorderLayout.WEST);
+      
 		
 		pane.setPreferredSize(new Dimension(200, 200));	
+		JScrollPane paneScrollPane1 = new JScrollPane(pane);
+        paneScrollPane1.setVerticalScrollBarPolicy(
+                        JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        paneScrollPane1.setPreferredSize(new Dimension(250, 155));
+        paneScrollPane1.setMinimumSize(new Dimension(10, 10));
+        frame.getContentPane().add(paneScrollPane1, BorderLayout.CENTER);
+      
 		
 		area.setVisible(true);
 		pane.setVisible(false);
@@ -45,21 +52,26 @@ public class TextAreaPane {
 		button.addActionListener(new java.awt.event.ActionListener(){ 
 		  public void actionPerformed(java.awt.event.ActionEvent evt) { 
 		  
-		    area.setVisible(false);
+		    int lines = area.getLineCount();
+			area.setVisible(false);	
 		    pane.setVisible(true);
-			int lines = area.getLineCount();
+			
 			try{// Traverse the text in the JTextArea line by line
 				for(int i = 0; i < lines; i ++){
 					int start = area.getLineStartOffset(i);
 					int end = area.getLineEndOffset(i);
 					// Implement method processLine
+					Document doc = pane.getDocument();
+					doc.insertString(doc.getLength(), area.getText(start, end-start), null);
 					System.out.println(area.getText(start, end-start));
 
 				}
 			}catch(BadLocationException e){
 				// Handle exception as you see fit
 			}
-			  } 
+			
+			
+			} 
 		} );
 		
 		

5d4109e2d8cacc20f8781a00ffa57dd48b3597ba
Jim Dai <jim.dai@autoliv.com>
2017-10-10 15:25:18 -0400

    Add extraxt special characteristic pairs

--- StringDisplayProc.java
+++ StringDisplayProc.java
@@ -3,18 +3,14 @@ import java.util.ArrayList;
 
 public class StringDisplayProc {
 	
-	public static String content, keyword;
+	public static String content, keyword, s1, s2;
 	
 	public static class ProcResult{
 		public int returnCode;
 		public String dispStr;    
 	}
 	
-
-	// public static class ProcResultArray{
-		// public List<ProcResult> searchFileResults = new ArrayList<String>();
-	// }
-	
+	//Extract Keyword
 	public static List<ProcResult> 	keywordExtract(String content, String keyword){
 		List<ProcResult> result = new ArrayList<ProcResult>();
 		result.clear();		
@@ -25,7 +21,6 @@ public class StringDisplayProc {
 				tempResult = new ProcResult();
 				tempResult.dispStr = content.substring(0, keyword.length());
 				tempResult.returnCode = 4;
-				System.out.println(tempResult.dispStr);
 				result.add(tempResult);
 				content = content.substring(keyword.length(), content.length());
 				pos = content.toLowerCase().indexOf(keyword.toLowerCase());
@@ -33,7 +28,6 @@ public class StringDisplayProc {
 				tempResult = new ProcResult();
 				tempResult.dispStr = content.substring(0, pos);
 				tempResult.returnCode = 1;
-				System.out.println(tempResult.dispStr);
 				result.add(tempResult);
 				content = content.substring(pos+1,content.length()) ;
 				pos = -1;
@@ -41,13 +35,11 @@ public class StringDisplayProc {
 				tempResult = new ProcResult();
 				tempResult.dispStr = content.substring(0, pos);
 				tempResult.returnCode = 1;
-				System.out.println(tempResult.dispStr);
 				result.add(tempResult);
 				
 				tempResult = new ProcResult();
 				tempResult.dispStr = content.substring(pos, pos+keyword.length());
 				tempResult.returnCode = 4;
-				System.out.println(tempResult.dispStr);
 				result.add(tempResult);
 				content = content.substring(pos+keyword.length(), content.length());
 				pos = content.toLowerCase().indexOf(keyword.toLowerCase());
@@ -62,16 +54,79 @@ public class StringDisplayProc {
 		return result;
 	}
 	
+	//Extract special characteritics, usaually the characteristics will be show up as a pair, such as "[" and "]"
+	public static List<Integer>  specialCharFind(String content, String s1, String s2 ){
+		List<Integer> result = new ArrayList<>();
+		result.clear();
+		
+		int pos1 = content.toLowerCase().indexOf(s1.toLowerCase());
+		int pos2 = content.toLowerCase().indexOf(s2.toLowerCase());
+		
+		if (pos1 < pos2) {			
+			result.add(pos1);
+			result.add(pos2);				
+		}
+		
+		return result;
+	}
+	
+	public static List<ProcResult> 	specialCharExtract(String content, String s1, String s2 ){
+		List<ProcResult> result = new ArrayList<ProcResult>();
+		result.clear();		
+		ProcResult tempResult;
+		
+		List<Integer> resultPos = specialCharFind(content, s1, s2 );
+		while (resultPos.size() > 0) {
+			
+			tempResult = new ProcResult();
+			tempResult.dispStr = content.substring(0,resultPos.get(0));
+			tempResult.returnCode = 1;
+			result.add(tempResult);
+			
+			tempResult = new ProcResult();
+			tempResult.dispStr = content.substring(resultPos.get(0), resultPos.get(0)+ s1.length());
+			tempResult.returnCode = 2;
+			result.add(tempResult);
+			
+			tempResult = new ProcResult();
+			tempResult.dispStr = content.substring(resultPos.get(0)+ s1.length(), resultPos.get(1));
+			tempResult.returnCode = 3;
+			result.add(tempResult);
+			
+			tempResult = new ProcResult();
+			tempResult.dispStr = content.substring(resultPos.get(1), resultPos.get(1)+ s2.length());
+			tempResult.returnCode = 2;
+			result.add(tempResult);
+			
+			content = content.substring(resultPos.get(1)+ s2.length(), content.length());
+			resultPos = specialCharFind(content, s1, s2 );
+			
+		}
+		
+		tempResult = new ProcResult();
+		tempResult.dispStr = content;
+		tempResult.returnCode = 1;
+		result.add(tempResult);
+		
+		return result;
+	
+		
+		
+		
+	}
+	
 
 	public static void main(String[] args) {
-		content = "mdam Test da";
+		content = "m<h>dam Test da </h>c";
 		//content = "da";
 		keyword = "da";
+		s1 = "<h>";
+		s2 = "</h>";
 		List<ProcResult> test = new ArrayList<ProcResult>();
-		test = keywordExtract(content, keyword);
+		test = specialCharExtract(content, s1, s2);
+		// test = keywordExtract(content, keyword);
 		for (int i=0; i < test.size(); i++){
-		//for (int i=0; i < 1; i++){
-			System.out.println(i + ":" + test.get(i).dispStr + ":" + test.get(i).returnCode);
+			System.out.println(test.get(i).dispStr + ":" + test.get(i).returnCode);
 		}
 		
 		
